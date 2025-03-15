7fb718cc67cc30fb9d08b59e4191106a7473c8fe
Khalifah Shabazz <shabazzk@gmail.com>
2025-03-15 00:48:56 -0400

    add: IAM Permission Policy Documents
    Attached (inline) IAM permission polices to IAM Roles.

--- circleci.tf
+++ circleci.tf
@@ -5,15 +5,16 @@ locals {
   circleci_configs = { for org, val in var.circleci :
     org => {
       client_id_list = [val.id] # because the provider URL requires the organization ID, making it unique, you cannot provide multiple client IDs.
-      iam_roles = {for p_name, prj in val.projects : p_name => {
-        path = "/OIDC/${local.circleci_name}/${org}/"
+      iam_roles = { for p_name, prj in val.projects : p_name => {
         assume_role_policy = templatefile("${path.module}/files/circleci-trust-policy.tftpl.json", {
           account_id      = local.account_id
           domain          = val.domain
           organization_id = val.id
           project_id      = prj.id
         })
-      }}
+        path       = "/OIDC/${local.circleci_name}/${org}/"
+        statements = prj.statements
+      } }
       provider_url = "https://${val.domain}/org/${val.id}"
     }
   }
--- modules/oidc/outputs.tf
+++ modules/oidc/outputs.tf
@@ -1,4 +1,10 @@
-output "iam_roles" {
-  description = "list of IAM roles"
-  value       = ""
+output "iam_oidc_arn" {
+  description = "ARN of an IAM OpenID Connect provider"
+  value = aws_iam_openid_connect_provider.oidc.arn
 }
+
+output "iam_role_arns" {
+  description = "OIDC provider IAM Roles"
+  value       = [for v in aws_iam_role.oidc : v.arn]
+}
+
